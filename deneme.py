e5a87dfd271258a2967831f0e5c0ae7242349965
AtakaNKuscu <119072059+AtakaNKuscu@users.noreply.github.com>
2022-11-25 20:49:06 +0300

    deneme.py via upload
    Me and my friend (@Tuxico-py) wrote a script that plays a song you want at first on spotify.

---
+++ deneme.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+isim = input("Şarkı: ")
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+class Spotify():
+    def pencere():
+        driver.get("http://www.spotify.com.tr")
+        assert "Spotify" in driver.title
+        driver.maximize_window()
+
+    def entrance():
+        giris = driver.find_element(By.CSS_SELECTOR, "button.jsmWVV")
+        giris.click()
+        time.sleep(3)
+        username = driver.find_element(By.ID, "login-username")
+        username.send_keys("atakankuscu35@hotmail.com")
+        password = driver.find_element(By.ID, "login-password")
+        password.send_keys("atAkan.3526")
+        tikla = driver.find_element(By.ID, "login-button")
+        tikla.click()
+        time.sleep(3)
+
+    def search():
+        elem = driver.find_element(By.LINK_TEXT, "Ara")
+        elem.click()
+        time.sleep(2)
+        elem2 = driver.find_element(By.CSS_SELECTOR, "input.QO9loc33XC50mMRUCIvf")
+        elem2.clear()
+        elem2.send_keys(isim)
+        elem2.send_keys(Keys.RETURN)
+        time.sleep(2)
+    def sarki():
+        song = driver.find_element(By.CSS_SELECTOR, "div.h4HgbO_Uu1JYg5UGANeQ")
+        song.click()
+        song2 = driver.find_element(By.CSS_SELECTOR, "button.Qs11Fsr_XqTVFDFWWRkQ")
+        song2.click()
+
+
+Spotify.pencere()
+Spotify.entrance()
+Spotify.search()
+Spotify.sarki()
